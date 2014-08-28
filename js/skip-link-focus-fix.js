9fc8770b8a090b91ebbfbdb59b98056332f23c4f
Silicon Forks <siliconforks@users.noreply.github.com>
2014-08-27 23:47:23 -0300

    Fix IE 8 support

--- js/skip-link-focus-fix.js
+++ js/skip-link-focus-fix.js
@@ -4,8 +4,20 @@
 	    is_ie     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1;
 
 	if ( ( is_webkit || is_opera || is_ie ) && document.getElementById) {
-		var eventMethod = ( window.addEventListener ) ? 'addEventListener' : 'attachEvent';
-		window[ eventMethod ]( 'hashchange', function() {
+		var eventMethod;
+		var eventName;
+		if ( window.addEventListener ) {
+			eventMethod = 'addEventListener';
+			eventName = 'hashchange';
+		}
+		else if ( window.attachEvent ) {
+			eventMethod = 'attachEvent';
+			eventName = 'onhashchange';
+		}
+		else {
+			return;
+		}
+		window[ eventMethod ]( eventName, function() {
 			var element = document.getElementById( location.hash.substring( 1 ) );
 
 			if ( element ) {
